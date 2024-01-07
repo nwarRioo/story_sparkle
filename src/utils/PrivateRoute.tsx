641c6216fed72202b9450839846d2219d8c20e01
Сатыбалдинов Ануар <paramode88@gmail.com>
2024-01-07 19:45:50 +0600

    #2 Добавление компонента с функионалом проверки доступа к страницам

---
+++ src/utils/PrivateRoute.tsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { AppState } from '../store/store';
+
+const PrivateRoute: React.FunctionComponent = (): React.ReactElement => {
+
+    const { isAuth } = useSelector((state: AppState) => state.auth, shallowEqual);
+    const location = useLocation();
+
+    return (
+        isAuth
+            ?
+            <Outlet />
+            :
+            <Navigate to='/login' replace state={{ from: location }} />
+    )
+};
+
+export default PrivateRoute;
