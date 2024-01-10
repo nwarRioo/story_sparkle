19dd58c9894feac0cc0f6f56e4345d03290b9b79
Сатыбалдинов Ануар <paramode88@gmail.com>
2024-01-10 23:44:06 +0600

    #4 Тестирование стилей

--- src/containers/UsersPage/UsersPage.tsx
+++ src/containers/UsersPage/UsersPage.tsx
@@ -1,14 +1,12 @@
 import { FunctionComponent, ReactElement, useEffect } from "react";
-// import { AppDispatch, AppState } from "../../store/store";
-// import { useDispatch, useSelector, shallowEqual } from "react-redux";
-// import UsersList from "../../components/UsersList/UsersList";
+import { AppDispatch, AppState } from "../../store/store";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import UsersList from "../../components/UsersList/UsersList";
 import { getUsers } from "../../store/users/users.slice";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../store/store";
 
 const UsersPage: FunctionComponent = (): ReactElement => {
     const dispatch: AppDispatch = useDispatch();    
-    // const { users, showError, errorMessage } = useSelector((state: AppState) => state.users, shallowEqual);
+    const { users, showError, errorMessage } = useSelector((state: AppState) => state.users, shallowEqual);
 
     useEffect(() => {
         dispatch(getUsers());
@@ -16,13 +14,13 @@ const UsersPage: FunctionComponent = (): ReactElement => {
 
     return (
         <div className="usersPage">
-            {/* <h2 className="usersPage__title">Авторы постов:</h2>
+            <h2 className="usersPage__title">Авторы постов:</h2>
             {showError ? <p>{errorMessage}</p> : null}
             {users === undefined || !users.length ?
                 <p className="usersPage__no_data">Авторов нет</p>
                 :
                 <UsersList users={users} />
-            } */}
+            }
         </div>
     );
 };
